c9707dbeb99586ee3d904825c65ee1cea6b9dbf2
BubblyBird <putrihamzah10@gmail.com>
2022-11-29 19:02:00 +0700

    UTS_NO2_C_3074

---
+++ src/main/java/com/mycompany/uts_no2_c_3074/Pegawai_3074.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.uts_no2_c_3074;
+
+/**
+ *
+ * @author LENOVO YOGA
+ */
+public class Pegawai_3074 {
+    protected String nama_3074, nip_3074;
+    protected int gaji_pokok_3074;
+    
+    public Pegawai_3074(String nama, String nip){
+        this.nama_3074=nama;
+        this.nip_3074 = nip;   
+    }
+    
+    public int HitungGajiAkhir_3074(){
+        return 0;
+    }
+    public void cetakPegawai_3074(){
+        System.out.println("NIP                 : "+nip_3074);
+        System.out.println("Nama                : "+nama_3074);
+    }   
+}
---
+++ src/main/java/com/mycompany/uts_no2_c_3074/UTS_NO2_C_3074.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.uts_no2_c_3074;
+
+/**
+ *
+ * @author LENOVO YOGA
+ */
+public class UTS_NO2_C_3074 {
+
+    public static void main(String[] args) {
+        SalariedEmployee_3074 S = new SalariedEmployee_3074("Budi","0001",300000);
+        CommissionEmployee_3074 C = new CommissionEmployee_3074("Dewi", "0002", 450000, 30000, 29 );
+        ProjectPlanner_3074 P = new ProjectPlanner_3074("Dio", "0003", 500000, 35000, 12);
+        
+        System.out.println("\n\n==DISPLAY DATA PEGAWAI==\n");
+        S.cetakSalariedEmployee_3074();
+        System.out.println();
+        C.cetakCommissionEmployee_3074();
+        System.out.println();
+        P.cetakProjectPlanner_3074();
+       
+    }
+}
